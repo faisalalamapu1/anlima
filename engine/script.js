67735ee989af36171210d51bd4bd5563962cb1be
faisalalamapu1 <52006981+faisalalamapu1@users.noreply.github.com>
2021-11-17 22:25:30 +0600

    Add files via upload

---
+++ engine/script.js
@@ -0,0 +1,42 @@
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+function save(){
+  var checkbox = document.getElementById('checkbox1zaal1');
+  localStorage.setItem('checkbox1zaal1', checkbox.checked);
+}
+
+function load(){    
+  var checked = JSON.parse(localStorage.getItem('checkbox1zaal1'));
+  document.getElementById("checkbox1zaal1").checked = checked;
+}
+
+load();
